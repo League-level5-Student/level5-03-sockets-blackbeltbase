34b0d5a216a96f01e46b3c7b460f34f4a748c94f
blackbeltbase <54255614+blackbeltbase@users.noreply.github.com>
2020-10-12 16:57:28 -0700

    Sockets

--- src/_01_Intro_To_Sockets/client/ClientGreeter.java
+++ src/_01_Intro_To_Sockets/client/ClientGreeter.java
@@ -1,18 +1,43 @@
 package _01_Intro_To_Sockets.client;
 
 import java.net.*;
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+import _00_Click_Chat.networking.Client;
+import _00_Click_Chat.networking.Server;
+
 import java.io.*;
 
 public class ClientGreeter {
-
+	static Server server;
+	Client client;
+	static String address;
+	public void start() {
+		 server = new Server(8080);
+		  address = server.getIPAddress();
+	}
    public static void main(String [] args) {
 	  //1. Create a String for the ip address of the server. 
 	  // If you don't know how to find a computer's ip address, ask about ifconfig on linux/mac and ipconfig on windows.
-      
+
       //2. Create an integer for the server's port number
-      
+      int portNum = 8080;
       //3. Surround steps 4-9 in a try-catch block that catches any IOExceptions.
-    
+    try {
+		Socket sock = new Socket(address,portNum);
+		DataOutputStream outStream = new DataOutputStream(sock.getOutputStream());
+		outStream.writeUTF("hello client");
+		DataInputStream inStream = new DataInputStream(sock.getInputStream());
+		System.out.println(inStream.readUTF());
+		JOptionPane.showMessageDialog(null, "Server started at: " + server.getIPAddress() + "\nPort: " + server.getPort());
+		server.start();
+		sock.close();
+		
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
     	 //4. Create an object of the Socket class. When initializing the object, pass in the ip address and the port number
  
          //5. Create a DataOutputStream object. When initializing it, use the Socket object you created in step 4 to call the getOutputStream() method.
